13c12de9d35932216f15b13b120eea5a515d16b0
annie tran <nntrn@me.com>
2020-09-04 02:08:23 -0500

    Updates

---
+++ precache-manifest.714fb713eedd97c95e72ec6e7995305d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c43d28230b50ae81c6c303c073e76b",
+    "url": "/pokecards/index.html"
+  },
+  {
+    "revision": "c360c4f7e74335a3748b",
+    "url": "/pokecards/static/css/main.4e366f2d.chunk.css"
+  },
+  {
+    "revision": "ac867bbe2cb097822188",
+    "url": "/pokecards/static/js/2.d97f2277.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokecards/static/js/2.d97f2277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c360c4f7e74335a3748b",
+    "url": "/pokecards/static/js/main.e14fe133.chunk.js"
+  },
+  {
+    "revision": "ff2ba52457a70785bd85",
+    "url": "/pokecards/static/js/runtime-main.6a71fb7b.js"
+  }
+]);
